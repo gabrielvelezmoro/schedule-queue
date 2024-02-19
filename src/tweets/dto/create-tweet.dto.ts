68e9d8b467700307546c5c066cc09b68aa211eec
gabriel <gabrieuvelezmoro@gmail.com>
2024-02-19 17:13:39 -0300

    feat: persiste e consulta tweets

--- src/tweets/dto/create-tweet.dto.ts
+++ src/tweets/dto/create-tweet.dto.ts
@@ -1,7 +1,3 @@
-export class Obj {
-  first_name: string;
-  last_name: string;
-}
 export class CreateTweetDto {
-  obj: Obj;
+  text: string;
 }
--- src/tweets/entities/tweet.entity.ts
+++ src/tweets/entities/tweet.entity.ts
@@ -1,1 +1,9 @@
-export class Tweet {}
+import { Column, Table, Model } from 'sequelize-typescript';
+
+@Table({ tableName: 'tweets' })
+export class Tweet extends Model {
+  @Column
+  text: string;
+}
+
+//biblioteca de persistence
