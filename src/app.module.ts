1f02a558f99230c9bfe519f22aec0c8c7c3bfaf5
gabriel <gabrieuvelezmoro@gmail.com>
2024-02-19 17:24:18 -0300

    feat: job executando a cada 5 segundos

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { DatabaseService } from './database/database.service';
 import { TweetsModule } from './tweets/tweets.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { join } from 'path';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'sqlite',
       host: join('database.sqlite'),
