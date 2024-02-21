47e0cc37018095a596360df4d048f406424ac0d1
gabriel <gabrieuvelezmoro@gmail.com>
2024-02-21 17:33:22 -0300

    feat: armazena quantidade de tweets no cache

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -1,17 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Tweet } from '../entities/tweet.entity';
 import { Interval } from '@nestjs/schedule';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class TweetsCountService {
+  private limit: any = 10;
   constructor(
     @InjectModel(Tweet)
-    private tweetModel: typeof Tweet,
+    private tweetsModel: typeof Tweet,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
 
   @Interval(5000)
-  async countTweets(): Promise<void> {
-    console.log('executou');
+  async countTweets() {
+    console.log('procurando tweets');
+
+    let offset = await this.cacheManager.get<number>('tweet-offset');
+    offset = offset === undefined ? 0 : offset;
+    const tweets: Tweet[] = await this.tweetsModel.findAll({
+      offset,
+      limit: this.limit,
+    });
+
+    console.log(`offset: ${offset}`);
+
+    if (tweets.length === this.limit)
+      this.cacheManager.set('tweet-offset', offset + this.limit, 1 * 60 * 10);
+
+    console.log(`achou ${tweets.length} tweets`);
   }
 }
